9acbfc3fc0f3ab72b8fa40ae41b481e9b992e10d
Caner Kocas <xtanakart@gmail.com>
2020-03-12 12:45:31 +0300

    commit

---
+++ todolist/src/ListItems.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './ListItems.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import FlipMove from 'react-flip-move';
+
+function ListItems(props) {
+    const items = props.items;
+    const listItems = items.map(item => {
+        return <div className="list" key={item.key}>
+                <p>
+                    <input type="text" id={item.key}
+                     value={item.text}
+                      onChange={(e) => {props.setUpdate(e.target.value, item.key)}}/>
+                <span>
+                    <FontAwesomeIcon className="faicons" 
+                    icon='trash'
+                    onClick={ () => props.deleteItem(item.key)}/>
+                    
+                </span>
+                
+                </p>
+
+        </div>
+    })
+    return (
+       <div>
+           <FlipMove duration={400} easing="ease-in">
+           {listItems}
+           </FlipMove>
+           </div> 
+    )
+        
+
+    
+}
+
+
+export default ListItems;
